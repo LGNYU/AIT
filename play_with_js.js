7bfdd4c806e396e48790b200c7cb4f2a81a5d88b
gly20030427 <104744218+gly20030427@users.noreply.github.com>
2023-09-11 16:00:25 -0400

    first play comit

---
+++ play_with_js.js
@@ -0,0 +1,12 @@
+let wordList = ['foo', 'bar', 'baz'];
+function main() {
+    wordList = [];
+    function wordList() {
+        // assume that a function exists called readFromDatabaseSync
+        const result = readFromDatabaseSync();
+        return result;
+    }
+    console.log(wordList);
+}
+main();
+console.log(wordList);
